32c0fa5a936edd43b391d05ec3a2bfb2fe43ce08
Gilga1412 <106699148+Gilga1412@users.noreply.github.com>
2023-01-01 18:12:22 +0700

    first commit

---
+++ resources/views/Register.blade.php
@@ -0,0 +1,6 @@
+@extends('MainBody.Main')
+
+@section('container')
+    <h1>Ini body register</h1>
+    {{--  <script src="js/script.js"></script>  --}} //<- ini cuman tanda darurat boleh dihapus jika mau
+@endsection
---
+++ routes/web.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/dull', function () {
+    return view('welcome');
+});
+//main route
+Route::get('/', function () {
+    return view('Home',[
+        'title' => 'Home',
+    ]);
+});
+Route::get('/register', function () {
+    return view('Register',[
+        'title' => 'Register',
+    ]);
+});
+Route::get('/login', function () {
+    return view('Login',[
+        'title' => 'Login',
+    ]);
+});
+
+
+//rumit
+Route::get('/category', function () {
+    return view('Category',[
+        'title' => 'Category',
+    ]);
+});
+
