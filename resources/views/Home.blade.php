189be0176861522c6c775670699201b174d986c0
Gilga1412 <106699148+Gilga1412@users.noreply.github.com>
2023-01-02 12:32:48 +0700

    first try commit!

--- resources/views/Home.blade.php
+++ resources/views/Home.blade.php
@@ -2,5 +2,6 @@
 
 @section('container')
     <h1>Ini body home</h1>
+    <h1>edit</h1>
     {{--  <script src="js/script.js"></script>  --}} //<- ini cuman tanda darurat boleh dihapus jika mau
 @endsection
