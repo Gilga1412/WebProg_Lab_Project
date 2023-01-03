1e7ec5182b789594822e5e8d2302827a638e24a0
Gilga1412 <106699148+Gilga1412@users.noreply.github.com>
2023-01-03 12:15:49 +0700

    ready commit!

--- resources/views/Login.blade.php
+++ resources/views/Login.blade.php
@@ -1,28 +1,28 @@
 @extends('MainBody.Main')
 
 @section('container')
-<h5>mark ini body login</h5>
+    <h5>mark ini body login</h5>
     <form action="/Home" enctype="multipart/form-data" method="GET">
-    @csrf
-    <h1>Login</h1>
-    <div class="LoginForm">
-        <Label>Email</Label>
-        <br>
-        <input type="email" name="email" placeholder="Enter Your Email">
-        <br>
-        <br>
-        <label for="Password">Password</label>
-        <br>
-        <input type="password" name="password" placeholder="Enter Your Password">
-        <br>
-    </div>
+        @csrf
+        <h1>Login</h1>
+        <div class="LoginForm">
+            <Label>Email</Label>
+            <br>
+            <input type="email" name="email" placeholder="Enter Your Email">
+            <br>
+            <br>
+            <label for="Password">Password</label>
+            <br>
+            <input type="password" name="password" placeholder="Enter Your Password">
+            <br>
+        </div>
 
 
-    <br>
+        <br>
         <div class="RememberMe">
             <div>
-            <input type="CheckBox" name="CheckList" checked={{ Cookie::get('mycookie') != null }}>
-            <h5 style="display: inline-block">Remember Me</h5>
+                <input type="CheckBox" name="CheckList" checked={{ Cookie::get('mycookie') != null }}>
+                <h5 style="display: inline-block">Remember Me</h5>
             </div>
         </div>
 
