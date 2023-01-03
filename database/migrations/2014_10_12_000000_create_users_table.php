4d709b04bc071365dae13d1c7bd717edf63c3e9f
Gilga1412 <106699148+Gilga1412@users.noreply.github.com>
2023-01-03 19:38:28 +0700

    !

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -23,6 +23,7 @@ class CreateUsersTable extends Migration
             $table->date('DOB');
             $table->string('Country');
             $table->rememberToken();
+            $table->timestamp('verified_at')->nullable();
             $table->timestamps();
         });
     }
